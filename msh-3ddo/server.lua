589b0f39da3049a28638828d7a176e7073665f98
MSHDEV <86056861+MSHDEV1@users.noreply.github.com>
2025-08-29 12:13:55 +0300

    Update server.lua

--- msh-3ddo/server.lua
+++ msh-3ddo/server.lua
@@ -1,15 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent('3ddo:shareDisplay')
-AddEventHandler('3ddo:shareDisplay', function(text, name)
-    TriggerClientEvent('3ddo:shareDisplay', -1, text, source, name)
-end)
-
-QBCore.Functions.CreateCallback('3ddo:sex', function(playerId, data) -- super
-    local Player = QBCore.Functions.GetPlayer(playerId)
-    
-    data({
-        first = Player.PlayerData.charinfo.firstname,
-        last = Player.PlayerData.charinfo.lastname
-    })
+RegisterServerEvent('msh-3ddo:shareDisplay')
+AddEventHandler('msh-3ddo:shareDisplay', function(text, name)
+    TriggerClientEvent('msh-3ddo:shareDisplay', -1, text, source, name)
 end)
