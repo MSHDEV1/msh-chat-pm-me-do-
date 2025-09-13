c17200bfd8a4b4865675d8490d6770f823b48f9f
MSHDEV <86056861+MSHDEV1@users.noreply.github.com>
2025-09-14 00:15:27 +0300

    Delete msh-3dme directory

--- msh-3dme/server.lua
+++
@@ -1,7 +0,0 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterServerEvent('msh-3dme:shareDisplay')
-AddEventHandler('msh-3dme:shareDisplay', function(text, name)
-    TriggerClientEvent('msh3dme:shareDisplay', -1, text, source, name)
-end)
-
